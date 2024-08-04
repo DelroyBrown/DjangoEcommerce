6f3841f8f09d74693eaa3013002e09f3eff07a99
Delroy Brown <delroybrown.db@gmail.com>
2024-08-04 17:42:56 +0100

    Updated Ecommerce_home/urls.py to include static file serving, and modified templates/home/home.html to include a new page layout with various sections and components

--- Ecommerce_home/urls.py
+++ Ecommerce_home/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = "Ecommerce_home"
@@ -6,3 +8,5 @@ app_name = "Ecommerce_home"
 urlpatterns = [
     path("", views.home, name="home"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
