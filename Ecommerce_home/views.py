b89991eb99ea3d6c55798b477ed3685b758f4aa9
Delroy Brown <delroybrown.db@gmail.com>
2024-08-04 18:03:58 +0100

    Updated home.html template and views.py to display products, removed unnecessary code, and reformatted HTML structure

--- Ecommerce_home/views.py
+++ Ecommerce_home/views.py
@@ -1,6 +1,8 @@
 # Ecommerce_home\views.py
 from django.shortcuts import render
+from Ecommerce_products.models import Product
 
 
 def home(request):
-    return render(request, "home/home.html")
+    all_products = Product.objects.all()
+    return render(request, "home/home.html", {"all_products": all_products})
